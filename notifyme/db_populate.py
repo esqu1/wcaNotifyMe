432d89e09351541b1ed4f3df5535a92c7fec3270
esqu1 <brandbest1@gmail.com>
2017-11-26 14:58:09 -0500

    refactoring and linting

--- notifyme/db_populate.py
+++ notifyme/db_populate.py
@@ -1,12 +1,12 @@
 from notifyme import app, db
-from .rssfetch import RSSParser
-from .models import Competition
-import datetime
+from .models import Competition, RSSParser
 from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
 
 sched = BackgroundScheduler()
 
-# Should be run as a cronjob every 12 hours.
+# Should be run as a cronjob.
+
 
 def populate_db():
     r = RSSParser()
@@ -17,16 +17,20 @@ def populate_db():
             if event not in strings:
                 if events[event][1] is not None:
                     if events[event][1] != 'open':
-                        dt = datetime.datetime.strptime(events[event][1], '%Y-%m-%dT%XZ')
+                        dt = datetime.datetime.strptime(events[event][1],
+                                                        '%Y-%m-%dT%XZ')
                     else:
                         dt = datetime.datetime.min
                         print dt
-                    new_event = Competition(name=event, website=events[event][0],
+                    new_event = Competition(name=event,
+                                            website=events[event][0],
                                             open_time=dt)
                 else:
-                    new_event = Competition(name=event, website=events[event][0],
+                    new_event = Competition(name=event,
+                                            website=events[event][0],
                                             open_time=None)
                 db.session.add(new_event)
         db.session.commit()
+    print "success!"
 
-sched.add_job(populate_db, 'interval', id="populate_db", seconds=10, misfire_grace_time=10)
+sched.add_job(populate_db, 'interval', id="populate_db", seconds=10)
