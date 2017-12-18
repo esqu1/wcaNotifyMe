bb805a2c600a6a617db86a84270a02eb4f733dcb
esqu1 <brandbest1@gmail.com>
2017-12-17 22:32:15 -0500

    oops that didn't work

--- notifyme/db_populate.py
+++ notifyme/db_populate.py
@@ -2,7 +2,6 @@ from notifyme import app, db
 from .models import Competition, RSSParser
 from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
-import logging
 
 sched = BackgroundScheduler()
 
@@ -38,4 +37,3 @@ def send_emails():
     pass
 
 sched.add_job(populate_db, 'interval', id="populate_db", seconds=10)
-logging.basicConfig(filename='log.txt', level=logging.INFO)
